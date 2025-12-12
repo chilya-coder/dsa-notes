1114f00e6504d4f4fc00cfffd7eb08f936a864c9
yuch <52803366+chilya-coder@users.noreply.github.com>
2025-12-12 22:21:23 +0000

    Time: 0 ms (100%), Space: 17.8 MB (52.03%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        currentIndex = 0
+        for i in nums:
+            if i != val:
+                nums[currentIndex] = i
+                currentIndex+= 1
+        return currentIndex
