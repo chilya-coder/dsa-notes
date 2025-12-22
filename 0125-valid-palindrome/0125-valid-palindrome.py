bbc30d6c5e5e5c7cc2be86654a08c962fe713e47
yuch <52803366+chilya-coder@users.noreply.github.com>
2025-12-22 22:09:41 +0000

    Time: 4 ms (90.78%), Space: 19.2 MB (26.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if len(s) == 1:
-            return True
-        new_str = re.sub(r'[\W_]', '', s)
-        new_str = new_str.lower()
-        arr_chr = []
-        for ch in new_str:
-            arr_chr.append(ch)
-        # 1. convert string to array of char
-        # 2. iterate from left and right to compare each char
-        # 3. we need n/2 iterations if length is even number and n/2 - 1 if odd
+        filteredString = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
 
-        for i in range (0, int(len(arr_chr)/2)):
-            if arr_chr[i] != arr_chr[len(arr_chr) - 1 - i]:
+        # swap approach, instead of the swap we compare
+
+        left,right = 0, len(filteredString) - 1
+
+        while (left < right):
+            if filteredString[left] != filteredString[right]:
                 return False
+            left += 1
+            right -= 1
         return True
+        
