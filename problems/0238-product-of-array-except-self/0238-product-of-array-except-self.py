c9336336d0c3662d37175f16f32f29ca80a05516
chilya-coder <yuliya.chimyrys@gmail.com>
2026-07-24 23:47:23 +0100

    Refactor productExceptSelf implementation for clarity; update README with pattern information

--- problems/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ problems/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # [1, 2, 3, 4]
-
-        #prefix 1[1, 2, 6, 12]
-        #postfix [24,24,12,4]1
-
-        #we want to optimize using prefix and postfix
-
         res = [1] * len(nums)
-        prefix = 1
+        # 1 2 4 6
 
+        # 1 1 2  8  48
+        #  48 48 24 6  1
+
+        prefix = 1
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        # [1, 1, 2, 6]
-        print(res)
+
         postfix = 1
-
-        for j in range (len(nums)-1, -1, -1):
-            res[j] *= postfix
-            postfix *= nums[j]
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+            
         return res
-
