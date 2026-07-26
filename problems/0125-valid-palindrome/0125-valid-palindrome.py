7da679bd3adfa6679b03f29683f37834dfb63d51
chilya-coder <yuliya.chimyrys@gmail.com>
2026-07-26 19:04:37 +0100

    Refactor palindrome check to use two-pointer technique for improved clarity

--- problems/0125-valid-palindrome/0125-valid-palindrome.py
+++ problems/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,33 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # filteredString = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        # Space Complexity: O(1)
+        # Time Complexity: O(N)
+        l_p, r_p = 0, len(s) - 1
 
-        # # swap approach, instead of the swap we compare
+        while l_p < r_p:
+            # ignores left non-alphanumeric characters
+            if not s[l_p].isalnum():
+                l_p += 1
+                continue
 
-        # left,right = 0, len(filteredString) - 1
-
-        # while (left < right):
-        #     if filteredString[left] != filteredString[right]:
-        #         return False
-        #     left += 1
-        #     right -= 1
-        # return True
-
-        # it's actually quite hard to remember the regex, so we can do it in one pass
-
-        # isalnum() -> checks if all the characters in a given string are alphanumeric
-        # we compare ONLY lower case letters and numbers (alphanumeric), rest - we skip
-
-        # Complexity: O(n) time, O(1) space (if we ignore the input string)
-        left, right = 0, len(s) - 1
-        while left < right:
-            while left < right and not s[left].isalnum():
-                left += 1
-            while left < right and not s[right].isalnum():
-                right -= 1
-
-            if s[left].lower() != s[right].lower():
+            if not s[r_p].isalnum():
+                r_p -= 1
+                continue
+            
+            if s[l_p].lower() != s[r_p].lower():
                 return False
-            left += 1
-            right -= 1
+
+            l_p += 1
+            r_p -= 1
+        
         return True
