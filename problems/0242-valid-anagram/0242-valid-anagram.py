fc94794a62dc7d28dbdeefecb338a02b5f2063c8
chilya-coder <yuliya.chimyrys@gmail.com>
2026-07-26 19:17:53 +0100

    Refactor isAnagram method for improved clarity and maintainability

--- problems/0242-valid-anagram/0242-valid-anagram.py
+++ problems/0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        #    A: sort both strings, traverse through them and if s[i] != t[i] return False
-        #    B: use HashMap and compare added values
-        #       In Pythonic way: return Counter(s) == Counter(t)
-        #    C: use HashMap and compare subtracted values
+        # Space Complexity: O(n)
+        # Time Complexity: O(n)
         if len(s) != len(t): return False
+        # return Counter(s) == Counter(t)
+
+        char_freq = defaultdict(int)
+
+        for c_s in s:
+            char_freq[c_s] += 1
         
-        key_value = dict()
-
-        for i in s:
-            if i in key_value:
-                key_value[i] = key_value.get(i) + 1
-            else:
-                key_value[i] = 1
-
-        for j in t:
-            if j not in key_value:
+        for c_t in t:
+            if c_t not in char_freq:
                 return False
-            elif key_value[j] <= 0:
-                return False
-            key_value[j] = key_value.get(j) - 1
+
+            char_freq[c_t] -= 1
+
+            if char_freq[c_t] == 0:
+                del char_freq[c_t]
         return True
