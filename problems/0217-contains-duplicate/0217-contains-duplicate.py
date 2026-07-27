808835fd628b6263b5957caea97ad633dd11ab88
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-07-27 20:36:13 +0100

    Correct variable name and add complexity notes
    Fixed variable name and added complexity comments.

--- problems/0217-contains-duplicate/0217-contains-duplicate.py
+++ problems/0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Space Complexity: O(n)
+        # Time Complexity: O(n)
+
+        # assume input list nums is UNSORTED, otherwise we can solve it with 2-pointers approach O(1)
         unique_set = set()
 
         for i in nums:
-            if i in my_set: return True
-            unique_set_set.add(i)
+            if i in unique_set: return True
+            unique_set.add(i)
         return False
