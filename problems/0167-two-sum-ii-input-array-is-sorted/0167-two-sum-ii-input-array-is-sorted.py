3dd35c00318108f542b704c18e1ec42fe4f24063
chilya-coder <yuliya.chimyrys@gmail.com>
2026-07-28 20:51:46 +0100

    Refactor twoSum function for clarity and consistency; remove redundant comments and improve variable naming

--- problems/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ problems/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        #  l  r  r  r
-        # [2,7,11,15]
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        l,r = 0, len(numbers) - 1
 
-        # The array is sorted, which means if I move pointers inward I can adjust the sum in a predictable way.
-        # I’ll place one pointer at the beginning and one at the end and compute their sum.
-        # If the sum is too small, I need a larger value, so I move the left pointer right.
-        # If the sum is too large, I move the right pointer left to decrease it.
-        
-        left, right = 0, len(numbers) - 1
-
-        while left < right:
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] > target:
-                right -=1
+        while r > l:
+            sum = numbers[r] + numbers[l] 
+            if sum == target:
+                return [l + 1, r + 1]
+            if sum > target:
+                r -= 1
             else:
-                left += 1
+                l += 1
+        return [l + 1, r + 1]
+        
