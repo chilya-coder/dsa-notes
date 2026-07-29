6117df03b0842857839a71c5b7a46b65e1f1dfa0
chilya-coder <yuliya.chimyrys@gmail.com>
2026-07-29 22:31:43 +0100

    Refactor MinStack implementation for improved readability and efficiency; streamline push and pop methods, and update comments.

--- problems/0155-min-stack/0155-min-stack.py
+++ problems/0155-min-stack/0155-min-stack.py
@@ -1,37 +1,29 @@
 class MinStack:
-
+    # Time Complexity: O(1)
+    # Space Complexity: O(n)
     def __init__(self):
         self.stack = []
         self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if not self.min_stack:
+        if not self.min_stack or val <= self.min_stack[-1]:
             self.min_stack.append(val)
         else:
-            if val < self.min_stack[-1]:
-                self.min_stack.append(val)
-            else:
-                self.min_stack.append(self.min_stack[-1])
+            self.min_stack.append(self.min_stack[-1])
 
-    def pop(self) -> None:
-        self.stack.pop(-1)
-        self.min_stack.pop(-1)
-
-    def top(self) -> int:
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        if self.min_stack:
-            return self.min_stack[-1]
-        else:
-            return None
+        #[1,2,0] # stack
+        #[1,1,0] # min stack
         
 
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
+        
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.min_stack[-1] if self.min_stack else None
