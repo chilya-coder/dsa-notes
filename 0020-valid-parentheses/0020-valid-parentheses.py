d19ee4871fce130231f9149c1bed5ed975ad0c9f
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-07-29 21:53:01 +0100

    Time: 0 ms (100%), Space: 19.2 MB (63.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        if len(s) == 1: return False
+        stack = []
+
+        bracket_map = {'(':')', '{': '}', '[': ']'}
+
+        for bracket_symbol in s:
+            if bracket_symbol in bracket_map:
+                stack.append(bracket_map[bracket_symbol])
+            elif len(stack) == 0 or bracket_symbol != stack.pop():
+                return False
+        return len(stack) == 0
