6b69ff342e2ebbd2f79dc9a08a9014c7ab43379e
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-08 21:04:42 +0100

    Time: 15 ms (44.57%), Space: 19.4 MB (46.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
         # anagram is the permutation -> we need to have exact same counter of characters
         # but different order
 
-        return Counter(s) == Counter(t)
+        #return Counter(s) == Counter(t)
+
+        s_map = Counter(s)
+
+        for char_t in t:
+            if char_t in s_map:
+                s_map[char_t] -= 1
+                if s_map[char_t] <= 0:
+                    del s_map[char_t]
+        print(s_map)
+        return len(s_map) == 0
