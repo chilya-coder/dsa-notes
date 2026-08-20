6c963287c68b22f5c90df066da9dfa3911e2a55d
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-20 19:31:58 +0100

    Time: 130 ms (96.19%), Space: 41.6 MB (58.59%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,14 +1,18 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position, speed), reverse = True)
-        fleets = []
+       # (0,1; 3,3; 5,1; 8,1; 10,2)
+       #  12     3    7    4     1
+       #
+       # 1. Sort input array desc by position and speed
+       # 2. If current time >= max_time - it forms a fleet.
 
-        for position, speed in cars:
-            time_left = (target - position) / speed
-            if not fleets:
-                fleets.append(time_left)
-            else:
-                if fleets[-1] < time_left: # it means our last car time is actually more than current, so we have fleet
-                    fleets.append(time_left)
-        return len(fleets)
-        
+
+        cars_sorted = sorted(zip(position, speed), reverse = True)
+        max_time = 0
+        fleets = 0
+        for pos, speed in cars_sorted:
+            time = (target - pos)/speed
+            if time > max_time:
+                fleets += 1
+                max_time = time
+        return fleets
