0b99338b1d22863cc4e92e72a3bb87f69148ac34
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-22 22:31:05 +0100

    Time: 75 ms (89.16%), Space: 19.6 MB (86.01%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,39 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # "A ABABBA" k = 2
+        #  lr
+        #  dict {A -> 3; B -> 3} for 6th iteration
+        #  1. A -> 1 - 1 <= k
+        #  2. AA -> 2 - 2 <= k
+        #  3. AAB -> 3 - 2 <= k
+        #  4. AABA -> 4 - 3 <= k
+        #  5. AABAB -> 5 - 3 <= k
+        #  6. AABABB -> 6 - 3 <= k - false, shrink
+        # --
+        # "AABABBA"
+        #.  lr
+        #  dict {A -> 3; B -> 3}
+        # ABABBA -> 6 - 3 <= k - false, shrink
+        # cycle ends r = len(s)
+
+        l = 0
+        freq_map = defaultdict(int)
+        max_freq = 0
+        max_len = 0
+
+        for r in range(len(s)):
+            # Upd counter for char 
+            freq_map[s[r]] += 1
+
+            # Upd max frequency in the sliding window
+            max_freq = max(max_freq, freq_map[s[r]])
+
+            # If we need > k shifts, shrink (move l)
+            window_len = r - l + 1
+            if window_len - max_freq > k:
+                freq_map[s[l]] -= 1
+                l += 1
+            max_len = max(max_len, r - l + 1)
+        return max_len
+            
+
