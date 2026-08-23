07531626e1d1927018bdc321de360371d112dbed
chilya-coder <yuliya.chimyrys@gmail.com>
2026-08-23 20:50:51 +0100

    Refactor 0003-longest-substring-without-repeating-characters solution and remove README; add 0424-longest-repeating-character-replacement solution and README

--- problems/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ problems/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,16 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # Time Complexity: O(n)
-        # Space Complexity: O(n)
-        window = set()
-        max_len = 0
-        l, r = 0, 0 # left is actual start of sequence, r just reads
-
-        while r < len(s):
-            while s[r] in window: #abca -> a is alr in window
-                window.remove(s[l])
+        # Space Complexity: O(1)
+        l = 0
+        max_substring = 0
+        unique_chars = set()
+        for r in range(len(s)):
+            while s[r] in unique_chars:
+                unique_chars.remove(s[l])
                 l += 1
-            window.add(s[r])
-            r += 1
-            max_len = max(max_len, r - l)
-        return max_len
+            unique_chars.add(s[r])
+            max_substring = max(max_substring, r - l + 1)
+        return max_substring
