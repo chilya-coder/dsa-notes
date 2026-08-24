a25f60da5eaee9112c2eb739647d70b6703d3267
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-24 19:13:43 +0100

    Time: 127 ms (33.71%), Space: 19.4 MB (97.74%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq_map = defaultdict(int)
+        l = 0
+        max_substring = 0
+        max_freq = 0
+
+        for r in range(len(s)):
+            # Update char frequency
+            freq_map[s[r]] += 1
+            max_freq = max(max_freq, max(freq_map.values()))
+            while r - l + 1 - max_freq > k:
+                freq_map[s[l]] -= 1
+                l += 1
+            max_substring = max(max_substring, r - l + 1)
+        return max_substring
