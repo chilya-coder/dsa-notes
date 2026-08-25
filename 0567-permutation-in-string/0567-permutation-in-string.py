871fc3d9a7678a89f904a122d2e524f9901e5bf0
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-25 21:48:03 +0100

    Time: 28 ms (39.28%), Space: 19.5 MB (18.57%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,19 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # Time Complexity: O(n)
-        # Space Complexity: O(n)
-        pattern = Counter(s1)
-        seen = defaultdict(int)
+        # Space Complexity: O(1)
+        freq_map = defaultdict(int)
         l = 0
+        pattern_map = Counter(s1)
         for r in range(len(s2)):
-            seen[s2[r]] += 1 
-                # {'c' -> 1}
-                # {'a' -> 2}
-            if r - l + 1 > len(s1):
-                seen[s2[l]] -= 1
-                if seen[s2[l]] == 0:
-                    del seen[s2[l]]
+            freq_map[s2[r]] += 1
+            while r - l + 1 > len(s1):
+                freq_map[s2[l]] -= 1
+                if freq_map[s2[l]] == 0:
+                    del freq_map[s2[l]]
                 l += 1
-            if seen and seen == pattern:
+            if pattern_map == freq_map:
                 return True
         return False
