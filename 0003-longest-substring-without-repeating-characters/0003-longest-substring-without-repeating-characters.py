79b3908a7f4829436346f9a6cb95fd96541c536c
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-25 21:02:31 +0100

    Time: 208 ms (32.55%), Space: 20 MB (5.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,15 +3,13 @@ class Solution:
         # Time Complexity: O(n)
         # Space Complexity: O(1)
         if len(s) == 0: return 0
-        l = 0
-        unique_substring = set()
-        max_len = 0
-        for r in range(len(s)):
-            while s[r] in unique_substring:
-                unique_substring.remove(s[l])
-                l += 1
-            max_len = max(max_len, r - l + 1)
-            unique_substring.add(s[r])
-        return max_len
 
-        
+        l, max_res = 0, 0
+        unique_chars = set()
+        for r in range(len(s)):
+            while s[r] in unique_chars:
+                unique_chars.remove(s[l])
+                l += 1
+            unique_chars.add(s[r])
+            max_res = max(max_res, r - l + 1)
+        return max_res
