639864c40a151dc471af82fb2c910b6aee39f80b
yuchi <52803366+insomniac-engineer@users.noreply.github.com>
2026-08-27 23:47:56 +0100

    Time: 0 ms (100%), Space: 20.5 MB (28.02%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,14 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # T O(n)
-    # M O(1)
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+        # # Iterative approach
+        # # Time Complexity: O(n)
+        # # Space Complexity: O(1)
+        # # Remove connection between 1 -> 2; save 2 to nxt
+        # # 1 (current) should be pointint on previous (null)
+        # curr, prev = head, None
+        # nxt = None
 
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        return prev
+        # while(curr):
+        #     nxt = curr.next
+        #     curr.next = prev # 1 -> null
+
+        #     prev = curr 
+        #     curr = nxt # 2
+
+        #return prev
+
+        # Recursive approach
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        if not head:
+            return None
+
+        newHead = head
+
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+        return newHead
+        
